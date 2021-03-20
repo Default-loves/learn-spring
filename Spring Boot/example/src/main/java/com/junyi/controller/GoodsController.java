f908668b6a82ed4f3e3558178bd45a817f684344
Default-loves <2111605130@mail2.gdut.edu.cn>
2021-03-20 18:03:43 +0800

    update

--- Spring Boot/example/src/main/java/com/junyi/controller/GoodsController.java
+++ Spring Boot/example/src/main/java/com/junyi/controller/GoodsController.java
@@ -54,4 +54,5 @@ public class GoodsController {
         return goodsRepository.findOne(specification).orElse(new Goods());
     }
 
+
 }
---
+++ Spring Boot/example/src/main/java/com/junyi/entity/Query.java
@@ -0,0 +1,17 @@
+package com.junyi.entity;
+
+import lombok.Data;
+
+/**
+ * @time: 2021/3/18 9:33
+ * @version: 1.0
+ * @author: junyi Xu
+ * @description:
+ */
+@Data
+public class Query {
+    //数据数量
+    private Integer pageSize;
+    //第几页
+    private Integer pageNum;
+}
